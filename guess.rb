21efc5e205d184377a6840ce95b966502691372c
Aaron Schumacher <ajschumacher@gmail.com>
2014-10-27 19:00:26 -0400

    hw 1 base sol.; exception for bad input

---
+++ guess.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+number = 5
+
+puts 'I am thinking of a number between 1 and 10.'
+
+5.times do
+  print 'GUESS MY NUMBER: '
+  guess = Integer(gets.chomp)
+  if guess < number
+    puts 'Too low!'
+  elsif number < guess
+    puts 'Too high!'
+  else
+    puts 'NAILED IT!!!'
+    exit
+  end
+end
+
+puts 'You lose! I win! Womp womp!'
